9a6c49c98064dbd23fad80bce335b0141c95e4f0
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-11 11:08:23 -0300

    Update 02.cs

--- Procedural/lógica/02.cs
+++ Procedural/lógica/02.cs
@@ -1,8 +1,8 @@
 // 02 - Faça um algoritmo que leia o valor do salário de um usuário,
 // calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado.
-// (Base para o Salário mínimo R$ 1.293,20). 
+// (Base para o Salário mínimo R$ 1.518,00). 
 
-double salarioMinimo = 1293.20;
+double salarioMinimo = 1518.00;
 
 Console.Write("Digite o valor do seu salário: R$ ");
 double salarioUsuario = Convert.ToDouble(Console.ReadLine());
