aeb386dafbe44f8002951b1f6ab75e83034eec8d
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-11 10:45:53 -0300

    Update 04.cs

--- Procedural/lógica/04.cs
+++ Procedural/lógica/04.cs
@@ -1,1 +1,16 @@
+// 04 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e
+// imprima na tela os valores. 
 
+Console.WriteLine("Digite o valor A: ");
+double vA = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor B: ");
+double vB = double.Parse(Console.ReadLine());
+
+double temp = vA;
+
+vA = vB;
+vB = temp;
+
+Console.WriteLine("Valor A: " +  vA);
+Console.WriteLine("Valor B: " + vB);
