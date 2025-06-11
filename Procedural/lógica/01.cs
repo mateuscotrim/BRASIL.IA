168aef13a6400cb9f02eb2a8bb237286c29d0612
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-11 10:17:58 -0300

    Update 01.cs

--- Procedural/lógica/01.cs
+++ Procedural/lógica/01.cs
@@ -1,1 +1,12 @@
+// 01 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor. 
 
+Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+   
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+
+     
+Console.WriteLine($"O antecessor de {numero} é {antecessor}.");
+Console.WriteLine($"O sucessor de {numero} é {sucessor}.");
