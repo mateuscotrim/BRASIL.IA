3331f2ea43bbe36954e003f7a3b656176da40bfc
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-11 10:53:49 -0300

    Update 03.cs

--- Procedural/lógica/03.cs
+++ Procedural/lógica/03.cs
@@ -1,8 +1,8 @@
 // 03 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%. 
 
 Console.Write("Digite um número qualquer: ");
-double numero = double.Parse(Console.ReadLine());
+double valor = double.Parse(Console.ReadLine());
 
-double calculo = numero + (numero * 0.05);
+double valorReajustado = valor + (valor * 0.05);
 
-Console.WriteLine("O valor digitado + um reajuste de 5%: " + calculo);
+Console.WriteLine("O valor digitado + um reajuste de 5%: " + valorReajustado);
