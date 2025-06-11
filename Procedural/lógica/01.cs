a4b40f551c806d068eadf7840fd39e868b1a7918
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-11 10:52:51 -0300

    Update 01.cs

--- Procedural/lógica/01.cs
+++ Procedural/lógica/01.cs
@@ -2,11 +2,9 @@
 
 Console.Write("Digite um número inteiro: ");
 int numero = int.Parse(Console.ReadLine());
-
    
 int antecessor = numero - 1;
 int sucessor = numero + 1;
 
-     
 Console.WriteLine($"O antecessor de {numero} é {antecessor}.");
 Console.WriteLine($"O sucessor de {numero} é {sucessor}.");
