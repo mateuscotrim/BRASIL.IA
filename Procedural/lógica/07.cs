eadc02e03a4c2af0007c0feb5acb5c6a5cd9396c
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-12 09:38:24 -0300

    Update 07.cs

--- Procedural/lógica/07.cs
+++ Procedural/lógica/07.cs
@@ -1,1 +1,7 @@
+// 07 - Faça um algoritmo que mostre um valor aleatório entre 0 e 100. 
 
+Random random = new Random();
+
+int valorAleatorio = random.Next(0, 101);
+
+Console.WriteLine($"Valor aleatório gerado: {valorAleatorio}");
