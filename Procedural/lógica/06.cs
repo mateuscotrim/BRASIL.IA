7f0df371187b1b252f609fae9c0801391a7c195a
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-12 09:36:43 -0300

    Update 06.cs

--- Procedural/lógica/06.cs
+++ Procedural/lógica/06.cs
@@ -1,1 +1,11 @@
+// 06 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente
+// em grau Celsius. Imprima na tela as duas temperaturas.
+// Fórmula: C = (5 * (F - 32) / 9)
 
+Console.WriteLine("Digite a temperatura em Fahrenheit: ");
+double tempFahrenheit = double.Parse(Console.ReadLine());
+
+double tempCelsius = (5 * (tempFahrenheit - 32) / 9);
+
+Console.WriteLine($"A temperatura em Fahrenheit: {tempFahrenheit}°F");
+Console.WriteLine($"A temperatura em Celsius: {tempCelsius:F2}°C");
