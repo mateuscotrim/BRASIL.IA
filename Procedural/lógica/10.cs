709f83635c41607b0cd05cfd2713c881b6ea6998
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-12 10:43:31 -0300

    Update 10.cs

--- Procedural/lógica/10.cs
+++ Procedural/lógica/10.cs
@@ -1,1 +1,16 @@
+// 10 - Faça um algoritmo que calcule a quanƟdade de litros de combusơvel gastos em uma viagem, sabendo
+// que o carro faz 12km com um litro. Deve-se fornecer ao usuário o tempo que será gasto na viagem a sua
+// velocidade média, distância percorrida e a quanƟdade de litros uƟlizados para fazer a viagem.
 
+Console.WriteLine("Digite a velocidade média (em km/h): ");
+double velocidadeMedia = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite quanto tempo durou a viagem (em horas): ");
+double tempoViagem = double.Parse(Console.ReadLine());
+
+double distancia = velocidadeMedia * tempoViagem;
+
+double litrosUsados = distancia / 12;
+
+Console.WriteLine($"Distância percorrida: {distancia} km");
+Console.WriteLine($"Quantidade de litros utilizados: {litrosUsados} litros");
