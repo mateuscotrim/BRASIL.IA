009874ce5a23e74061b3bc6d34b40208ef477980
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-12 09:42:42 -0300

    Update 08.cs

--- Procedural/lógica/08.cs
+++ Procedural/lógica/08.cs
@@ -1,1 +1,14 @@
+// 08 - Faça um algoritmo que leia dois valores inteiros A e B, imprima na tela o quociente e o resto da divisão
+// inteira entre eles. 
 
+Console.WriteLine("Digite o valor de A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B: ");
+int B = int.Parse(Console.ReadLine());
+
+int quociente = A / B;
+int resto = A % B;
+
+Console.WriteLine($"O quociente de {A} dividido por {B} é: {quociente}");
+Console.WriteLine($"O resto da divisão de {A} por {B} é: {resto}");
