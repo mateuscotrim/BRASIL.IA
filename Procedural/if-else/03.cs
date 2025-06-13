814f2e369487747423ee38ea9bb7bca437ab5af5
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 10:50:18 -0300

    Update 03.cs

--- Procedural/if-else/03.cs
+++ Procedural/if-else/03.cs
@@ -1,1 +1,19 @@
+// 03 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá
+// somar os dois valores, caso contrário devera mulƟplicar A por B. 
+// Ao final de qualquer um dos cálculos devese atribuir o resultado a uma variável C e imprimir seu valor na tela. 
 
+Console.WriteLine("Digite o valor de A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B: ");
+int B = int.Parse(Console.ReadLine());
+
+int C; 
+
+if (A == B) {
+    C = A + B;
+} else {
+    C = A * B;
+}
+
+Console.WriteLine("O resultado é: " + C);
