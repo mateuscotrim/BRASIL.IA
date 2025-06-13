9490ec96f98365720b21a8f0c92b871f13e20c4b
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 11:07:02 -0300

    Update 04.cs

--- Procedural/if-else/04.cs
+++ Procedural/if-else/04.cs
@@ -1,1 +1,17 @@
+// 04 - Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO. 
 
+Console.WriteLine("Digite TRUE ou FALSE: ");
+
+bool A = bool.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite TRUE ou FALSE: ");
+bool B = bool.Parse(Console.ReadLine());
+
+if (A == true && B == true) {
+    Console.WriteLine("Ambos são VERDADEIROS");
+} else if (A == false && B == false)
+{
+    Console.WriteLine("Ambos são FALSOS");
+} else {
+    Console.WriteLine("Um é VERDADEIRO e o outro é FALSO");
+}
