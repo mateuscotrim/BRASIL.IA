87c080892d6dafd7be488cfa967fac6c48cdc124
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 10:48:14 -0300

    Update 02.cs

--- Procedural/if-else/02.cs
+++ Procedural/if-else/02.cs
@@ -1,1 +1,19 @@
+// 02 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar,
+// posiƟvo ou negaƟvo
 
+Console.WriteLine("Digite um número: ");
+double numeroQualquer = double.Parse(Console.ReadLine());
+
+if (numeroQualquer > 0) {
+    Console.WriteLine("O número é positivo.");
+} else if (numeroQualquer < 0) {
+    Console.WriteLine("O número é negativo.");
+} else {
+    Console.WriteLine("O número é zero.");
+}
+
+if (numeroQualquer % 2 == 0) {
+    Console.WriteLine("O número é par.");
+} else {
+    Console.WriteLine("O número é ímpar.");
+}
