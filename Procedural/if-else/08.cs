1cce2c09534e5bf448642da59825495ae0d6165f
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 11:18:55 -0300

    Update 08.cs

--- Procedural/if-else/08.cs
+++ Procedural/if-else/08.cs
@@ -1,1 +1,28 @@
+// 08 - Faça um algoritmo que leia quatro notas obƟdas por um aluno, calcule a média das nota obƟdas,
+// imprima na tela o nome do aluno e se o aluno foi aprovado ou reprovado. Para o aluno ser considerado
+// aprovado sua média final deve ser maior ou igual a 7. 
 
+Console.WriteLine("Digite o nome do aluno: ");
+string nomeAluno = Console.ReadLine();
+
+Console.WriteLine("Digite a primeira nota: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota: ");
+double nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a terceira nota: ");
+double nota3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a quarta nota: ");
+double nota4 = double.Parse(Console.ReadLine());
+
+double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+Console.WriteLine($"Média do aluno {nomeAluno}: {media:F2}");
+
+if (media >= 7) {
+    Console.WriteLine("Aluno aprovado!");
+} else {
+    Console.WriteLine("Aluno reprovado.");
+}
