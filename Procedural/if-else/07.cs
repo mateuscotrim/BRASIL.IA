a0ffad032caf2f72037a818d824319aea29959ce
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 11:16:21 -0300

    Update 07.cs

--- Procedural/if-else/07.cs
+++ Procedural/if-else/07.cs
@@ -1,1 +1,14 @@
+// 07 - Faça um algoritmo que leia três notas obƟdas por um aluno, e imprima na tela a média das notas
 
+Console.WriteLine("Digite a primeira nota: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota: ");
+double nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a terceira nota: ");
+double nota3 = double.Parse(Console.ReadLine());
+
+double media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"A média das notas é: {media:F2}");
