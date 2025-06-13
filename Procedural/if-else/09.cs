48ab6d6c021b0df385d161cefbe9fa4f1785df40
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 11:20:10 -0300

    Update 09.cs

--- Procedural/if-else/09.cs
+++ Procedural/if-else/09.cs
@@ -1,1 +1,13 @@
+// 09 - Faça algoritmo que leia o nome e a idade de uma peso e imprima na tela o nome da pessoa e se ela é maior ou menor de idade.
 
+Console.WriteLine("Digite o nome da pessoa: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite a idade da pessoa: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 18) {
+    Console.WriteLine($"{nome} é maior de idade.");
+} else {
+    Console.WriteLine($"{nome} é menor de idade.");
+}
