78984d560f94e7e62d6475896e8b2fc7f9aa7eef
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-13 11:22:32 -0300

    Update 10.cs

--- Procedural/if-else/10.cs
+++ Procedural/if-else/10.cs
@@ -1,1 +1,23 @@
+//  10 - Faça um algoritmo que leia três valores que representam os três lados de um triângulo e
+//  verifique se são válidos, determine se o triângulo é: equilátero, isósceles ou escaleno. 
 
+Console.WriteLine("Digite o primeiro lado do triângulo: ");
+double lado1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo lado do triângulo: ");
+double lado2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro lado do triângulo: ");
+double lado3 = double.Parse(Console.ReadLine());
+
+if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) {
+    if (lado1 == lado2 && lado2 == lado3) {
+        Console.WriteLine("O triângulo é Equilátero.");
+    } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        Console.WriteLine("O triângulo é Isósceles.");
+    } else {
+        Console.WriteLine("O triângulo é Escaleno.");
+    }
+} else {
+    Console.WriteLine("Os valores fornecidos não formam um triângulo válido.");
+}
