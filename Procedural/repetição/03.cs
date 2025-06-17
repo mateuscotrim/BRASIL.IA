b64b0177d1eedbb7d86ac44cae8498a6d81d1176
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-17 10:46:25 -0300

    Update 03.cs

--- Procedural/repetição/03.cs
+++ Procedural/repetição/03.cs
@@ -1,1 +1,10 @@
+// 03 - Faça um algoritmo que receba um valor inteiro e imprima na tela a sua tabuada 
 
+Console.Write("Digite um número para ver a tabuada: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabuada do {n}:");
+
+for (int i = 1; i <= 10; i++) {
+    Console.WriteLine($"{n} x {i} = {n * i}");
+}
