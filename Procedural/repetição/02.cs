8acdd5e99a23936c5db943b524e74b10958b4342
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-17 10:44:26 -0300

    Update 02.cs

--- Procedural/repetição/02.cs
+++ Procedural/repetição/02.cs
@@ -1,9 +1,9 @@
 // 02 - Faça um algoritmo que imprima na tela a tabuada de 1 até 10. 
 
-for (int i = 1; i <= 10; i++) {
-    Console.WriteLine($"Tabuada do {i}:");
+for (int n = 1; n <= 10; n++) {
+    Console.WriteLine($"Tabuada do {n}:");
     for (int t = 1; t <= 10; t++) {
-        Console.WriteLine($"{t} x {i} = {i * t}");
+        Console.WriteLine($"{t} x {n} = {n * t}");
     }
     Console.WriteLine(); 
 }
