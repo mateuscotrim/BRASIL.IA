eb908c9d11304526d95d59d3dc36b09120b1a8e5
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2025-06-17 10:23:08 -0300

    Update 01.cs

--- Procedural/repetição/01.cs
+++ Procedural/repetição/01.cs
@@ -1,1 +1,17 @@
+// 01 - Sara tem 1,50m e cresce 2 cenơmetros por ano, enquanto Francisco tem 1,10m e cresce 3 cenơmetros
+// por ano. Faça um algoritmo que calcule e imprima na tela em quantos anos serão necessários para que
+// Francisco seja maior que Sara.
+
+double sara = 1.50;
+double francisco = 1.10;
+int anos = 0;
+
+while (sara >= francisco)
+{
+    sara += 0.02;
+    francisco += 0.03;
+    anos++;
+}
+
+Console.WriteLine($"É necessario {anos} anos para Francisco ser maior que a Sara.");
 
